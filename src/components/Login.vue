83ff6d6964bbe28087b318bb77dcccf4e71dd508
pengjin0 <827879040@qq.com>
2017-06-01 16:19:33 +0800

    form验证

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,48 +1,22 @@
 <template>
-
-	<!--<router-link to='index'>
-		index
-	</router-link>-->
-	<!--<router-link to=''>
-		home
-	</router-link>
-	<router-link to=''>
-		links
-	</router-link>-->
-
 	<div id="login">
 		<div class="login">
-			<form  class="el-form demo-ruleForm login-container el-form--label-left">
-				<h3  class="title">系统登录</h3>
-				<div  class="el-form-item is-required">
-					
-					<div class="el-form-item__content" style="margin-left: 0px;">
-						<div  class="el-input">
-							
-							<input type="text" placeholder="账号" autocomplete="off" class="el-input__inner">
-							
-						</div>
-						
-					</div>
-				</div>
-				<div  class="el-form-item is-required">
-					
-					<div class="el-form-item__content" style="margin-left: 0px;">
-						<div  class="el-input">
-							<input type="password" placeholder="密码" autocomplete="off" class="el-input__inner">							
-						</div>
-					</div>
-				</div> <label  class="el-checkbox remember"><span class="el-checkbox__input is-checked"><span class="el-checkbox__inner"></span><input type="checkbox" class="el-checkbox__original" value=""></span><span class="el-checkbox__label">记住密码<!----></span></label>
-				<div  class="el-form-item" style="width: 100%;">
-				
-					<div class="el-form-item__content" style="margin-left: 0px;">
-						<button  @click="login_delu" type="button" class="el-button el-button--primary" style="width: 100%;">
-							
-						<span>登录</span></button>
-						
-					</div>
-				</div>
-			</form>
+			<h4 class="title">登录系统</h4>
+			<el-form :model="ruleForm" :rules="rules" ref="ruleForm" class="demo-ruleForm">
+				  <el-form-item label="账号" prop="name">
+				    <el-input style="width: 300px;" placeholder="请输入用户名" v-model="ruleForm.name"></el-input>
+				  </el-form-item>
+				  <el-form-item label="密码" prop="password">
+				    <el-input style="width: 300px;" placeholder="请输入密码" v-model="ruleForm.password"></el-input>
+				  </el-form-item>
+				  
+				  <el-checkbox style="margin: 10px 0 20px 10px;" v-model="ruleForm.checked">记住密码</el-checkbox>
+
+				  <el-form-item>
+				    <el-button style="width: 100%;"  type="primary" @click="submitForm('ruleForm')">登录</el-button>
+				    <!--<el-button @click="resetForm('ruleForm')">重置</el-button>-->
+				  </el-form-item>
+			</el-form>
 		</div>
 
 	</div>
@@ -50,15 +24,43 @@
 </template>
 
 <script>
-	//vuejs逻辑
-	export default {
-	    methods: {
-	      login_delu() {//登录的时候打开一条消息
-	        this.$message('这是一条消息提示');
-	      }
-	    }
+  export default {
+    data() {
+      return {
+        ruleForm: {
+          name: '',
+          password: '',
+          checked:true
+        },
+        rules: {
+          name: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 6, max: 25, message: '密码长度在6个字符以上', trigger: 'blur' }
+          ],
+        }
+      };
+    },
+    methods: {
+      submitForm(formName) {
+        this.$refs[formName].validate((valid) => {
+          if (valid) {//验证之后的处理
+          this.$message('这是一条消息提示');
+            alert('submit!');
+          } else {//出错的处理
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      resetForm(formName){
+        this.$refs[formName].resetFields();
+      }
+    }
   }
-	
 </script>
 
 <style>
